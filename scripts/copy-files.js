c5a40306eb0155ee5f3ae1f824a01ea485970b3f
Oleg Slobodskoi <oleg008@gmail.com>
2017-02-10 14:09:19 +0100

    merge from next

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -49,7 +49,7 @@ function createPackageFile() {
     } = packageData;
 
     const minimalPackage = {
-      name: 'material-ui',
+      name: '@ubergrape/material-ui',
       author,
       version,
       description,
